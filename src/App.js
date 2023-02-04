cbbd971a9cbaf9c3198f571ce2d8c200812cefa0
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-04 10:48:03 +0900

    add all files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className="app">
       <Layout>
         <First />
-        <Second />
       </Layout>
     </div>
   );
--- src/components/first/first.js
+++ src/components/first/first.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./first.css"
+import { Link } from 'react-router-dom'
 
 function first() {
     return (
         <div className='first'>
-            first!!!!!
+            <h1 className='first_pageTitle'>1日1名言</h1>
+            <p className='first__page__text1'>
+                こちらのサイトでは1日1つの名言をご覧いただくことができます。
+            </p>
+            <p className='first__page__text2'>
+                以下のボタンをクリックし今日の一言をぜひご覧ください。
+            </p>
+            <p className='first__page__text3'>
+                今日も名言を見て乗り切りましょう
+            </p>
+            <div>
+                <a href="#">今日の名言</a>
+            </div>
         </div>
     )
 }
